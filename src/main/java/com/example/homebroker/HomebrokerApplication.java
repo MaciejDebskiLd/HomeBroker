19d537a88916ea0083586f1bd9c0d1e2a864d368
Latitude <patrycjaadutkiewicz@gmailcom>
2019-10-05 15:39:51 +0200

    app

--- src/main/java/com/example/homebroker/HomebrokerApplication.java
+++ src/main/java/com/example/homebroker/HomebrokerApplication.java
@@ -1,12 +1,24 @@
 package com.example.homebroker;
 
+import com.example.homebroker.utils.BootStrap;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class HomebrokerApplication {
 
+	private BootStrap bootStrap;
 
+	public HomebrokerApplication(BootStrap bootStrap){
+		this.bootStrap = bootStrap;
+	}
+
+	@PostConstruct
+	public void init(){
+		bootStrap.initializeRepository();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomebrokerApplication.class, args);
